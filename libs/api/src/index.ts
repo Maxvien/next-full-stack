8f34ded2f5b519e931d2415ffda5838192fcd187
Vien Dinh <maxvien@gmail.com>
2021-09-19 19:57:47 +0700

    remove root.router.ts

--- libs/api/src/index.ts
+++ libs/api/src/index.ts
@@ -1,6 +1,10 @@
-import { RootRouter } from './routers/root.router';
+import { createRouter } from './core';
+import { PostRouter } from './routers/post.router';
+import { UserRouter } from './routers/user.router';
 
-export const router = RootRouter;
-export type Router = typeof RootRouter;
+export const router = createRouter()
+  .merge('post.', PostRouter)
+  .merge('user.', UserRouter);
 
+export type Router = typeof router;
 export { createContext, createExpressMiddleware } from './core';
--- libs/api/src/routers/root.router.ts
+++
@@ -1,7 +0,0 @@
-import { createRouter } from '../core';
-import { PostRouter } from './post.router';
-import { UserRouter } from './user.router';
-
-export const RootRouter = createRouter()
-  .merge('post.', PostRouter)
-  .merge('user.', UserRouter);
